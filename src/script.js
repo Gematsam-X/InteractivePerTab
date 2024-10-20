7340f77cc9b41d7462131a1314894fdd0f939029
Gionatan Venturi <gionatan.venturi@gmail.com>
2024-10-20 16:08:45 +0200

    Aggiunta logica di base

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// Seleziona tutti gli elementi <td>
+let elements = document.getElementsByTagName("td");
+
+// Crea un array vuoto per gli elementi con classi non vuote
+let filteredElements = [];
+
+// Itera su ciascun elemento <td>
+for (let i = 0; i < elements.length; i++) {
+    // Controlla se la classe non è vuota e non è "special"
+    if (elements[i].className !== "" && elements[i].className !== "special") {
+        // Aggiungi l'elemento all'array filtrato
+        filteredElements.push(elements[i]);
+        
+        // Aggiungi un evento di click per il reindirizzamento
+        elements[i].addEventListener('click', function() {
+            // Estrai il simbolo dall'elemento
+            const symbol = elements[i].innerHTML.split('<br>')[1]; // Ottieni il simbolo dall'elemento
+            // Reindirizza alla pagina dell'elemento
+            window.location.href = "elements/html/" + symbol.toLowerCase() + ".html";
+        });
+    }
+}
