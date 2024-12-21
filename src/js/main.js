1b4f2dffe482b64a331902790179ed0dbc0a657b
Gematsam X <165379022+Gematsam-X@users.noreply.github.com>
2024-12-21 19:09:09 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -538,6 +538,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Gestione del bottone per attivare/disattivare la visualizzazione avanzata
 advancedVisualizzationButton.addEventListener("click", () => {
-  document.body.style.transition = "background-color 0.5s ease");
+  document.body.style.transition = "background-color 0.5s ease";
   toggleStatus(true)
 })
