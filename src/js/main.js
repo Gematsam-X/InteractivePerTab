56bb70b18cb36bd2820c349835a6a86715848835
Gionatan Venturi <gionatan.venturi@gmail.com>
2024-12-21 11:16:54 +0100

    Implementazione di advancedVisualizzation UNSTABLE

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,8 @@
 // Aggiungi un evento per eseguire l'estrazione dei dati quando la pagina è pronta
 window.addEventListener("load", function () {
-  estraiDatiElementi()
-    .then(() => {
-      console.log("Dati estratti con successo!");
-    })
-    .catch((error) => {
-      console.error("Errore durante l'estrazione dei dati:", error);
-    });
+  estraiDatiElementi().catch((error) => {
+    console.error("Errore durante l'estrazione dei dati:", error);
+  });
 });
 
 // Seleziona tutti gli elementi <td>
@@ -16,26 +12,33 @@ let elements = document.getElementsByTagName("td");
 let filteredElements = [];
 
 // Itera su ciascun elemento <td>
-for (let i = 0; i < elements.length; i++) {
-  // Controlla se la classe non è vuota e non è "special", "legend" o "specialLegend"
-  if (
-    elements[i].classList.length > 0 && // Aggiungiamo il controllo sulla lunghezza delle classi
-    !elements[i].classList.contains("special") &&
-    !elements[i].classList.contains("legend") &&
-    !elements[i].classList.contains("specialLegend")
-  ) {
-    // Aggiungi l'elemento all'array filtrato
-    filteredElements.push(elements[i]);
+function checkElementsClass() {
+  for (let i = 0; i < elements.length; i++) {
+    // Controlla se la classe non è vuota e non è "special", "legend", "no-border" o "specialLegend"
+    if (
+      elements[i].classList.length > 0 &&
+      !elements[i].classList.contains("special") &&
+      !elements[i].classList.contains("legend") &&
+      !elements[i].classList.contains("specialLegend") &&
+      !elements[i].classList.contains("no-border")
+    ) {
+      // Aggiungi l'elemento all'array filtrato
+      filteredElements.push(elements[i]);
 
-    // Aggiungi un evento di click per il reindirizzamento
-    elements[i].addEventListener("click", function () {
-      elements[i].style.transform = "scale(1.2)"; // Ingrandisce l'elemento al clic
-      // Estrai il simbolo dall'elemento
-      const symbol = elements[i].innerHTML.split("<br>")[1]; // Ottieni il simbolo dall'elemento
-      // Reindirizza alla pagina dell'elemento
-      window.sessionStorage.removeItem("currentElement");
-      window.location.href = "elements/html/" + symbol.toLowerCase() + ".html";
-    });
+      // Aggiungi un evento di click per il reindirizzamento
+      elements[i].addEventListener("click", function () {
+        elements[i].style.transform = "scale(1.2)"; // Ingrandisce l'elemento al clic
+        // Estrai il simbolo dall'elemento
+        const symbol = elements[i].innerHTML.split("<br>")[1]; // Ottieni il simbolo dall'elemento
+        // Reindirizza alla pagina dell'elemento
+        window.sessionStorage.removeItem("currentElement");
+        window.setTimeout(() => {
+          resetDefaultStyle();
+        }, 500);
+        window.location.href =
+          "elements/html/" + symbol.toLowerCase() + ".html";
+      });
+    }
   }
 }
 
@@ -76,6 +79,7 @@ function estraiDatiElementi() {
     // Se i dati sono stati trovati, salva nella variabile globale e risolvi la Promise
     if (datiTemporanei.length > 0) {
       datiElementi = datiTemporanei; // Popola la variabile globale
+      console.log(datiElementi);
       resolve(); // Risolve la Promise
     } else {
       reject("Nessun dato trovato");
@@ -133,6 +137,19 @@ document
 
 // Definisci le categorie
 const metals = document.getElementById("metalsLegendContainer");
+const transitionMetals = document.getElementById(
+  "transitionMetalsLegendContainer"
+);
+const internalTransitionMetals = document.getElementById(
+  "internalTransitionMetalsLegendContainer"
+);
+const postTransitionMetals = document.getElementById(
+  "postTransitionMetalsLegendContainer"
+);
+const alkaliMetals = document.getElementById("alkaliMetalsLegendContainer");
+const alkalineEarthMetals = document.getElementById(
+  "alkalineEarthMetalsLegendContainer"
+);
 const nonMetals = document.getElementById("nonMetalsLegendContainer");
 const metalloids = document.getElementById("metalloidsLegendContainer");
 const artificials = document.getElementById("artificialsLegendContainer");
@@ -168,80 +185,82 @@ function adjustTransparency(targetClass) {
 let activeCategoryRange = null; // Variabile per tracciare la categoria attiva
 
 // Funzione per evidenziare una categoria specifica e gestire il toggle
-function highlightCategoryRange(start, end, category) {
-  const elements = document.querySelectorAll("td");
+function highlightCategoryRange(category, series) {
+  const allElements = document.querySelectorAll("td");
 
-  // Se la categoria è già attiva, rimuovi la classe "faded" e resetta
   if (activeCategoryRange === category) {
-    elements.forEach((element) => {
+    // Se la categoria è già attiva, rimuoviamo 'faded' da tutti gli elementi
+    allElements.forEach((element) => {
       element.classList.remove("faded");
     });
-    activeCategoryRange = null; // Resetta la categoria attiva
-  } else {
-    // Altrimenti evidenzia gli elementi nell'intervallo
-    elements.forEach((element) => {
-      const contenuto = element.innerHTML.trim();
-      const righeContenuto = contenuto.split("<br>");
-
-      // Se è un numero di gruppo o periodo, non deve essere evidenziato
-      if (element.classList.contains("group-period"))
-        element.classList.add("faded");
-
-      if (righeContenuto.length >= 2) {
-        const elementNumber = parseInt(righeContenuto[0]);
-
-        if (elementNumber >= start && elementNumber <= end) {
-          element.classList.remove("faded"); // Rimuovi la trasparenza
-        } else {
-          element.classList.add("faded"); // Aggiungi la trasparenza
-        }
-      }
-    });
-    activeCategoryRange = category; // Imposta la categoria attiva
+    activeCategoryRange = null; // Annulla la categoria attiva
+    return; // Termina la funzione, niente più evidenza
   }
-}
 
-// Funzione per rimuovere la classe "faded" da tutti gli actinides
-function removeFadedFromActinides() {
-  const actinidesElements = document.querySelectorAll(".actinides");
-  actinidesElements.forEach((element) => {
-    element.classList.remove("faded"); // Rimuovi la classe "faded" da tutti gli actinides
+  // Aggiungi una classe 'faded' a tutti gli elementi
+  allElements.forEach((element) => {
+    element.classList.add("faded");
   });
+
+  // Poi rimuovi 'faded' da quelli che appartengono alla categoria specificata
+  const selectedElements = document.querySelectorAll(`.${series}`);
+  selectedElements.forEach((element) => {
+    element.classList.remove("faded"); // Rimuovi il 'faded' per la serie selezionata
+  });
+
+  activeCategoryRange = category; // Imposta la categoria come attiva}
 }
 
-// Funzione per rimuovere la classe "faded" da tutti i lanthanides
-function removeFadedFromLanthanides() {
-  const lanthanidesElements = document.querySelectorAll(".lanthanides");
-  lanthanidesElements.forEach((element) => {
-    element.classList.remove("faded"); // Rimuovi la classe "faded" da tutti i lanthanides
+// Funzione per rimuovere la classe 'faded' da un gruppo specifico di elementi
+function removeFadedFromCategory(categoryClass) {
+  const categoryElements = document.querySelectorAll(categoryClass);
+  categoryElements.forEach((element) => {
+    element.classList.remove("faded"); // Rimuovi la classe 'faded' dal gruppo di elementi
   });
 }
 
 // Eventi per i Lantanoidi
 lanthanides.forEach((lantanoide) => {
   lantanoide.addEventListener("click", function () {
-    highlightCategoryRange(57, 71, "57-71"); // Evidenzia i Lantanoidi
-    removeFadedFromLanthanides();
+    highlightCategoryRange("57-71", "lanthanid"); // Evidenzia i Lantanoidi
+    removeFadedFromCategory(".lanthanides, .lanthanid"); // Rimuove 'faded' dai Lantanoidi
   });
 });
 
 // Eventi per gli Attinoidi
 actinides.forEach((actinoide) => {
   actinoide.addEventListener("click", function () {
-    highlightCategoryRange(89, 103, "89-103"); // Evidenzia gli Attinoidi
-    removeFadedFromActinides();
+    highlightCategoryRange("89-103", "actinid"); // Evidenzia gli Attinoidi
+    removeFadedFromCategory(".actinides, .actinid"); // Rimuove 'faded' dagli Attinoidi
   });
 });
 
-// Eventi per ogni categoria
-metals.addEventListener("click", () => adjustTransparency("metal"));
+// Aggiungi eventi per ogni categoria per controllare la trasparenza
+metals.addEventListener("click", () => {
+  adjustTransparency("metal");
+  removeFadedFromCategory(".lanthanides, .actinides");
+});
+internalTransitionMetals.addEventListener("click", () => {
+  adjustTransparency("internalTransitionMetal");
+});
+transitionMetals.addEventListener("click", () =>
+  adjustTransparency("transitionMetal")
+);
+postTransitionMetals.addEventListener("click", () => {
+  adjustTransparency("postTransitionMetal");
+});
+alkalineEarthMetals.addEventListener("click", () => {
+  adjustTransparency("alkalineEarthMetal");
+});
+
+alkaliMetals.addEventListener("click", () => {
+  adjustTransparency("alkaliMetal");
+});
 nonMetals.addEventListener("click", () => adjustTransparency("non-metal"));
 metalloids.addEventListener("click", () => adjustTransparency("metalloid"));
 artificials.addEventListener("click", () => adjustTransparency("artificial"));
 nobleGases.addEventListener("click", () => adjustTransparency("noble-gas"));
-
 // Funzioni per il pulsante "Mostra nella tavola periodica"
-
 // Gestisci la selezione dell'elemento corrente nella tavola periodica
 const currentElementSymbol = window.sessionStorage.getItem("currentElement");
 
@@ -295,3 +314,198 @@ if (currentElementSymbol) {
     }
   });
 }
+
+//* VISUALIZZAZIONE AVANZATA
+
+// Stato della visualizzazione avanzata (attiva/disattiva)
+let isAdvancedVisualizzation = false;
+
+// Elementi principali del DOM
+const advancedVisualizzationButton = getCategoryCell("advancedVisualizzation"); // Bottone per visualizzazione avanzata
+const metalsLegendContainerCell = getCategoryCell("metalsLegendContainer"); // Contenitore principale delle legende
+
+// Selezione dei contenitori per categorie specifiche di metalli
+const metalsCategoriesLegendContainer = [
+  getCategoryCell("transitionMetalsLegendContainer"),
+  getCategoryCell("alkalineEarthMetalsLegendContainer"),
+  getCategoryCell("alkaliMetalsLegendContainer"),
+  getCategoryCell("postTransitionMetalsLegendContainer"),
+  getCategoryCell("internalTransitionMetalsLegendContainer"),
+];
+
+// Seleziona tutti gli elementi che devono essere nascosti quando attiviamo la visualizzazione avanzata
+let removableElements = document.querySelectorAll(".removable");
+
+// --- FUNZIONI UTILI ---
+
+/**
+ * Restituisce l'elemento DOM con un dato id
+ * @param {string} category ID dell'elemento da selezionare
+ * @returns {HTMLElement|null} Elemento trovato o null
+ */
+function getCategoryCell(category) {
+  return document.querySelector(`#${category}`);
+}
+
+/**
+ ** Funzione principale per attivare/disattivare la visualizzazione avanzata
+ * @param {boolean} removeMetalClass Indica se rimuovere le classi dei metalli
+ */
+function toggleStatus(removeMetalClass) {
+  // Modifica il testo del bottone in base allo stato corrente
+  advancedVisualizzationButton.innerText = isAdvancedVisualizzation
+    ? "Disattiva visualizzazione dettagliata"
+    : "Attiva visualizzazione dettagliata";
+
+  if (isAdvancedVisualizzation) {
+    // **Attivazione visualizzazione avanzata**
+    metalsLegendContainerCell.classList.add("hidden"); // Nasconde il contenitore dei metalli generico
+    metalsCategoriesLegendContainer.forEach((category) =>
+      category.classList.remove("hidden")
+    ); // Mostra contenitori per categorie specifiche
+
+    // Aggiunge classi specifiche a lantanoidi e attinoidi
+    document
+      .querySelectorAll(".lanthanid, .actinid, .lanthanides, .actinides")
+      .forEach((obj) => {
+        obj.classList.add("internalTransitionMetal");
+        if (
+          obj.classList.contains("lanthanides") ||
+          obj.classList.contains("actinides")
+        ) {
+          obj.classList.add("no-click");
+        }
+      });
+
+    datiElementi.forEach((element) => {
+      // Verifica che 'element.number' sia definito e che possa essere convertito in numero
+      const numeroAtomico = parseInt(element.number);
+
+      // Controlla che il numero atomico sia valido e inferiore a 112
+      if (!isNaN(numeroAtomico) && numeroAtomico < 112) {
+        if (element.number) {
+          filteredElements.forEach((obj) => {
+            let firstRow = obj.innerHTML.trim().split("<br>");
+            // Verifica che l'elemento DOM esista
+            if (
+              (parseInt(firstRow) >= 21 && parseInt(firstRow) <= 30) ||
+              (parseInt(firstRow) >= 39 && parseInt(firstRow) <= 48) ||
+              (parseInt(firstRow) >= 72 && parseInt(firstRow) <= 80) ||
+              (parseInt(firstRow) >= 104 && parseInt(firstRow) <= 112)
+            ) {
+              obj.classList.add("transitionMetal");
+            }
+            if (
+              (parseInt(firstRow) == 3 ||
+                parseInt(firstRow) == 11 ||
+                parseInt(firstRow) == 19 ||
+                parseInt(firstRow) == 37 ||
+                parseInt(firstRow) == 55 ||
+                parseInt(firstRow) == 87) &&
+              !obj.classList.contains("group-period")
+            ) {
+              obj.classList.add("alkaliMetal");
+            }
+            if (
+              (parseInt(firstRow) == 4 ||
+                parseInt(firstRow) == 12 ||
+                parseInt(firstRow) == 20 ||
+                parseInt(firstRow) == 38 ||
+                parseInt(firstRow) == 56 ||
+                parseInt(firstRow) == 88) &&
+              !obj.classList.contains("group-period")
+            ) {
+              obj.classList.add("alkalineEarthMetal");
+            }
+            if (
+              (parseInt(firstRow) == 13 &&
+                !obj.classList.contains("group-period")) ||
+              parseInt(firstRow) == 31 ||
+              parseInt(firstRow) == 49 ||
+              parseInt(firstRow) == 50 ||
+              (parseInt(firstRow) >= 81 && parseInt(firstRow) <= 83) ||
+              (parseInt(firstRow) >= 113 && parseInt(firstRow) <= 116)
+            ) {
+              obj.classList.add("postTransitionMetal");
+            }
+          });
+        } else {
+          console.warn(
+            `Elemento DOM non trovato per il numero atomico ${element.number}`
+          );
+        }
+      }
+    });
+
+    // Nasconde gli elementi "rimovibili"
+    removableElements.forEach((element) => element.classList.add("hidden"));
+  } else {
+    //** Disattivazione visualizzazione avanzata
+    metalsLegendContainerCell.classList.remove("hidden"); // Mostra il contenitore principale
+
+    // Nasconde i contenitori delle categorie specifiche
+    metalsCategoriesLegendContainer.forEach((category) =>
+      category.classList.add("hidden")
+    );
+
+    // Rimuove classi specifiche da lantanoidi e attinoidi
+    if (removeMetalClass) {
+      document.querySelectorAll(".internalTransitionMetal").forEach((obj) => {
+        if (!obj.classList.contains("artificial"))
+          obj.classList.remove("internalTransitionMetal");
+        if (obj.id === "internalTransitionMetalsLegendContainer")
+          obj.classList.add("internalTransitionMetal");
+
+        if (
+          obj.classList.contains("lanthanides") ||
+          obj.classList.contains("actinides")
+        ) {
+          obj.classList.remove("no-click");
+        }
+      });
+
+      document.querySelectorAll(".transitionMetal").forEach((obj) => {
+        if (obj.id != "transitionMetalsLegendContainer")
+          obj.classList.remove("transitionMetal");
+      });
+
+      document.querySelectorAll(".postTransitionMetal").forEach((obj) => {
+        if (obj.id != "postTransitionMetalsLegendContainer")
+          obj.classList.remove("postTransitionMetal");
+      });
+
+      document.querySelectorAll(".alkaliMetal").forEach((obj) => {
+        if (obj.id != "alkaliMetalsLegendContainer")
+          obj.classList.remove("alkaliMetal");
+      });
+      document.querySelectorAll(".alkalineEarthMetal").forEach((obj) => {
+        if (obj.id != "alkalineEarthMetalsLegendContainer")
+          obj.classList.remove("alkalineEarthMetal");
+      });
+    }
+
+    // Mostra nuovamente gli elementi nascosti
+    removableElements.forEach((element) => element.classList.remove("hidden"));
+  }
+
+  // Inverti lo stato della variabile globale
+  isAdvancedVisualizzation = !isAdvancedVisualizzation;
+}
+
+// --- EVENT LISTENERS ---
+
+/**
+ * Event listener principale al caricamento del DOM:
+ * 1. Estrae i dati e controlla le classi.
+ * 2. Imposta la visualizzazione predefinita.
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  estraiDatiElementi(); // Estrae i dati
+  checkElementsClass(); // Controlla le classi sugli elementi
+  toggleStatus(false); // Configurazione visualizzazione iniziale
+});
+
+// Gestione del bottone per attivare/disattivare la visualizzazione avanzata
+advancedVisualizzationButton.addEventListener("click", () =>
+  toggleStatus(true)
+);
