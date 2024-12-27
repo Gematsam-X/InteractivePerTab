fcc0c6f0fe72b6b0897c22135045f4ce5c7737c0
Gionatan Venturi <gionatan.venturi@gmail.com>
2024-12-27 16:59:27 +0100

    Migliorata gestione oggetti

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,4 +1,5 @@
-document.body.classList.add("no-transition");
+const body = document.body;
+body.classList.add("no-transition");
 
 function showContentAfterDelay() {
   window.setTimeout(() => {
@@ -7,21 +8,22 @@ function showContentAfterDelay() {
 }
 
 export const toggleButton = document.querySelector(".theme-toggle");
-const body = document.body;
-
 // Verifica se l'utente ha già impostato un tema (chiaro o scuro)
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark-theme");
 }
 
 // Funzione di toggle per il cambio di tema
-toggleButton.addEventListener("click", () => {
+function toggleTheme() {
   body.style.transition = "background-color 0.5s ease";
   body.classList.toggle("dark-theme");
   const theme = body.classList.contains("dark-theme") ? "dark" : "light";
   localStorage.setItem("theme", theme);
-});
+}
 
+if (toggleButton) {
+  toggleButton.addEventListener("click", toggleTheme);
+}
 window.addEventListener("DOMContentLoaded", () => {
   showContentAfterDelay();
 });
