9f43395b8e3bb97f02c578f701e3cf98d1c20c2a
Gematsam X <165379022+Gematsam-X@users.noreply.github.com>
2024-12-30 22:27:51 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -414,6 +414,7 @@ function toggleStatus(removeSpecificClass) {
   advancedViewButton.innerText = isAdvancedView
     ? "Disattiva visualizzazione avanzata"
     : "Attiva visualizzazione avanzata";
+  body.style.transition = "background-color 0.5s ease"; // Add a transition effect for the background color
 
   if (isAdvancedView) {
     // **Enable advanced view**
